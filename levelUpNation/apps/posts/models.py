c4d857bee82599c399ca916e3306b1ee60105292
Andres Yasir Romero <andyasromero@gmail.com>
2024-09-28 13:45:04 -0300

    add models

---
+++ levelUpNation/apps/posts/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    icono = models.ImageField(upload_to="media/usuarios", null=True, blank=True)
+    descripcion = models.TextField(max_length=350)
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+        
+    
+class Categorias(models.Model):
+    nombre = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        db_table = "Categorias"
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+
+class Posts(models.Model):
+    titulo = models.CharField(max_length=250, verbose_name="Titulo")
+    contenido = models.TextField(verbose_name="Contenido")
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to="media/posts", null=True, blank=True)
+    
+    def __str__(self):
+        return self.titulo
+    
+    class Meta:
+        db_table = "Post"
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        
+
+class Comentarios(models.Model):
+    fecha = models.DateTimeField(auto_now_add=True)
+    contenido = models.TextField(max_length=250, verbose_name="Contenido")
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+
+
+
+# class Imagenes(models.Model):
+#     imagen = models.ImageField(upload_to="/media/posts")
+#     post = models.ForeignKey(Posts, on_delete=models.CASCADE)
--- levelUpNation/levelUpNation/settings/base.py
+++ levelUpNation/levelUpNation/settings/base.py
@@ -19,25 +19,25 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zru@w($6qs$rmil5*2246y*av-s-(@qh5tric58yo=2k8ul9vv"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+DJANGO_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+    'apps.posts',
+]
+
+THIRD_APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -70,17 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "levelUpNation.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -97,9 +86,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es-ar"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Argentina/Buenos_Aires"
 
 USE_I18N = True
 
@@ -115,3 +104,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Modified User
+AUTH_USER_MODEL = "posts.User"
+
+# Path Media Files
+# MEDIA_URL = "/media/"
