b5e0756e1e2ae44fe9c7f71f4ef84cf4fa49ca8f
Andres Yasir Romero <andyasromero@gmail.com>
2024-09-30 12:15:04 -0300

    organizando carpetas

--- levelUpNation/apps/posts/views.py
+++ levelUpNation/apps/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Posts
 
-# Create your views here.
+# Vista Basada en Funciones
+
+def posts(request):
+    ctx = {}
+    noticias = Posts.objects.all()
+    ctx["noticias"] = noticias
+    return render(request, "posts/index.html", ctx)
+
+
+def login(request):
+    return render(request, "usuarios/login.html")
