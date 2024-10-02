a2331c06b0f96d76a2587df3dfd3a0a0c8cfc0a2
ricardo enrique hortt <ricardohortt@gmail.com>
2024-10-02 20:46:35 -0300

    modificacion css/urls/templates

--- levelUpNation/apps/posts/urls.py
+++ levelUpNation/apps/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="noticias"),
-    path("post/", views.post, name="post"),
+    path("", views.index, name="inicio"),
+    path("post/", views.post, name="noticias"),
     path("login/", views.login, name="login")
 ]
