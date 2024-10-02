ab1cdfe9052223e7ad742d12e01c125f69be4585
Facundo Chifflet <[200~>
2024-10-02 18:11:44 -0300

    modificando templates/url/css

--- levelUpNation/apps/posts/urls.py
+++ levelUpNation/apps/posts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="noticias"),
-    path("post/", views.post, name="post")
+    path("post/", views.post, name="post"),
+    path("login/", views.login, name="login")
 ]
