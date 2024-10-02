9468314b11069f885165df96cafacb5db22468d5
Facundo Chifflet <[200~>
2024-10-02 18:12:51 -0300

    nueva view/img

--- levelUpNation/apps/posts/views.py
+++ levelUpNation/apps/posts/views.py
@@ -10,3 +10,7 @@ def index(request):
 def post(request):
     template = loader.get_template("post.html")
     return HttpResponse(template.render({}, request))
+
+def login(request):
+    template = loader.get_template("login.html")
+    return HttpResponse(template.render({}, request))
