996adb95e4d1534af40666c6e0669d3dedd24161
Andres Yasir Romero <andyasromero@gmail.com>
2024-10-05 19:00:01 -0300

    add filtro por categoria

--- levelUpNation/apps/posts/views.py
+++ levelUpNation/apps/posts/views.py
@@ -7,6 +7,15 @@ def index(request):
     latest_posts_list = Posts.objects.order_by("-fecha_publicacion")
     template = loader.get_template("home.html")
     context = {"title":"Level Up Nation"}
+    if request.POST:
+        status = request.POST["categoria"]
+        if status != "ALL":
+            latest_posts_list = Posts.objects.filter(categoria__nombre=status).order_by("-fecha_publicacion")
+            # if not latest_posts_list:
+            #     return HttpResponse("La categoria seleccionada aún no tiene posteos.")
+        
+            
+        
     context["latest_posts_list"] = latest_posts_list
     return HttpResponse(template.render(context, request))
 
